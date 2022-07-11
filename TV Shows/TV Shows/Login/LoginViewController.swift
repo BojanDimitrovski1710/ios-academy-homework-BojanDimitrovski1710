91e5ac45d5ddfeee3d635779138e0c3047a1502d
BojanDimitrovski <bojan@dimitrovski.net>
2022-07-11 16:26:44 +0200

    Homework 1, Finished

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -5,15 +5,38 @@
 //  Created by Infinum on 11.07.2022..
 //
 
+var counter = 0
+
 import UIKit
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var counterLbl: UILabel!
+    @IBOutlet weak var incrementBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        counter = 0
+        counterLbl.text = " "
         print("Test")
     }
     
-    @IBAction func touchButton(){
+    @IBAction func touchIncrement(){
         print("Whats Up?")
+        counter += 1
+        print(counter)
+        counterLbl.text = String(counter)
+    }
+    
+    @IBAction func touchDecrement(){
+        print("Whats Down?")
+        counter -= 1
+        print(counter)
+        counterLbl.text = String(counter)
+    }
+    
+    @IBAction func touchReset(){
+        print("Whats Back At The Start?")
+        counter = 0
+        print(counter)
+        counterLbl.text = String(counter)
     }
 }
