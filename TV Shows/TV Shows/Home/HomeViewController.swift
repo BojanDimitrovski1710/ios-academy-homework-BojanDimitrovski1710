8dd102b9a5011b731e58c480254c57ac516b22bf
BojanDimitrovski <bojan@dimitrovski.net>
2022-07-20 12:40:36 +0200

    Added push to Home view from login and register

---
+++ TV Shows/TV Shows/Home/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  TV Shows
+//
+//  Created by Infinum on 11.07.2022..
+//
+
+
+
+import UIKit
+final class HomeViewController: UIViewController {
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Outlets
+
+    
+
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func setupUI(){
+        
+    }
+    
+    // MARK: - Actions
+    
+    
+    
+}
