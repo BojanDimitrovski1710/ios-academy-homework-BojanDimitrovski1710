4a748ebedd9d765fc85d9c08ad1703bfb72303dc
BojanDimitrovski <bojan@dimitrovski.net>
2022-08-03 13:30:50 +0200

    Committing everything

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import Alamofire
 import MBProgressHUD
 final class LoginViewController: UIViewController {
-
+    
     // MARK: - Properties
     
     private var counter: Int = 0
@@ -75,7 +75,8 @@ final class LoginViewController: UIViewController {
             rememberUser = false
         }
         
-    }    
+    }
+    
     
     @IBAction func revertEmailToNormal(_ sender: Any) {
         emailField.layer.borderWidth = 0
